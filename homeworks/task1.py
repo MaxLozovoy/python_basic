9f4f0e9e4e3883e9c30ccdeddae8ebeef31afeee
MaxLozovoy <maxim-lozovoy17@yandex.ru>
2020-11-03 14:06:21 +0300

    homework les 3, changed task1.py, task5.py

--- homeworks/task1.py
+++ homeworks/task1.py
@@ -4,8 +4,8 @@
 """
 
 
-number1 = int(input('Введите первое число:\n>>>'))
-number2 = int(input('Введите второе число числа:\n>>>'))
+number1 = float(input('Введите первое число:\n>>>'))
+number2 = float(input('Введите второе число числа:\n>>>'))
 
 
 def div_numbers(number1, number2):
@@ -14,7 +14,9 @@ def div_numbers(number1, number2):
         result = number1 / number2
         return result
     except ZeroDivisionError:
-        return 'На ноль делить нельзя'
+        print('На ноль делить нельзя')
+        return float('nan')
+        # выдает не ошибку а то, что полученный результат неопределенное число
 
 
 print('Ответ: ', round(div_numbers(number1, number2), 2))
--- homeworks/task5.py
+++ homeworks/task5.py
@@ -8,4 +8,27 @@
  то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 """
 
+def insert_sum(*args):
+    result = 0
+    exit_flag = False
+    for itm in args:
+        try:
+            result += float(itm) if itm else 0
+            # если itm пустая строка, то itm 0
+        except ValueError as e:
+            if itm == 'q':
+                exit_flag = not exit_flag
+                break
+    return result, exit_flag
 
+
+user_sum = 0
+while True:
+    user_input = input('введите числа через пробел\n>>>').split(' ')
+    result_summ, user_exit = insert_sum(*user_input)
+    user_sum += result_summ
+    print(f'сумма: {user_sum}')
+
+    if user_exit:
+        print('Досвидания')
+        break
