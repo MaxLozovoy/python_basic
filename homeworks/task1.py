e72a5be70540d85b2beb92e3b8587162a3e584e2
MaxLozovoy <maxim-lozovoy17@yandex.ru>
2020-11-07 14:14:52 +0300

    homework les 5

---
+++ homeworks/task1.py
@@ -0,0 +1,26 @@
+"""
+Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+Об окончании ввода данных свидетельствует пустая строка.
+
+"""
+
+my_file = open('file.txt', 'w', encoding='UTF-')
+
+while True:
+    line = input('Введите строку \n>>>')
+    if line == ' ':
+        break
+    my_file.write(line)
+
+my_file.close()
+
+my_file = open('file.txt', 'r', encoding='UTF-')
+content = my_file.readlines()
+print(content)
+
+my_file.close()
+
+
+
+
+
